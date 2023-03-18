cb172501a5dd77a0a8b649ecefdd274199338f81
Eddahcherotich <88654343+Eddah-cipher@users.noreply.github.com>
2023-03-18 18:34:36 -0400

    Decentralized Storage
    This is a Decentralized Storage which allows the end user to upload a file to the IPFS system and record its ownership into the blockchain.
     Intro to Ethereum and blockchain.

---
+++ build/precache-manifest.0af5a59ae6bd552fe7e4c18d07d4b939.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0ed3074d947a5d7843f16e23bf88a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0514bb0b2a8dacf26985",
+    "url": "./static/css/2.e1f817c4.chunk.css"
+  },
+  {
+    "revision": "88b64a50919a3489e06d",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "0514bb0b2a8dacf26985",
+    "url": "./static/js/2.cf9d1f8d.chunk.js"
+  },
+  {
+    "revision": "ae38674da3d478bdfc1a14b68ee45921",
+    "url": "./static/js/2.cf9d1f8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88b64a50919a3489e06d",
+    "url": "./static/js/main.50cabc10.chunk.js"
+  },
+  {
+    "revision": "43690a3716bff23d547f",
+    "url": "./static/js/runtime-main.6bbdc2f3.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "./static/media/box.df8b84d5.png"
+  }
+]);
